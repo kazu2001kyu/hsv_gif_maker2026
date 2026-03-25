6476d2526e28159446f999da7c691087e42437d1
kazu2001kyu <154589951+kazu2001kyu@users.noreply.github.com>
2026-03-25 15:14:38 +0900

    Update gif_maker.py
    hsv_gif_maker2026を追加する

--- gif_maker.py
+++ gif_maker.py
@@ -5,13 +5,15 @@ import os
 pictures = []
 
 # ファイルの保管されているフォルダー名
-DIR = 'fig_8_check'
+DIR = '
+hsv_gif_maker2026/fig_8_check'
 file_count = sum(os.path.isfile(os.path.join(DIR, name))
                 for name in os.listdir(DIR))
 
 # 画像を箱に入れていく pic_nameにて画像を保存しているフォルダーとファイル名を参照している. 200~270(最後の275はカウントされない).pngを参照して合成
 for count in range(200,275,5):
-    pic_name = 'fig_8_check/'+str(count) + '.png'
+    pic_name = '
+hsv_gif_maker2026/fig_8_check/'+str(count) + '.png'
     img = Image.open(pic_name)
     pictures.append(img)
 # gifアニメを出力する
